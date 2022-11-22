76d8c29d1abf691f505be550063c3dee6a5bd5dc
jnatmed <juanmannat@gmail.com>
2022-11-22 13:55:54 -0300

    dando interaccion al mapa(X,Y) con javascript

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-// var map = L.map('map').setView([-34.64568, -58.39916], 13);
-var map = L.map('map').setView(new L.LatLng(-34.64568, -58.39916), 18);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+const btn_localizar = document.querySelector("#buscar")
+
+
+function localizar() {
+    var X = document.getElementById("latitud").value;
+    var Y = document.getElementById("longitud").value;
+    
+    // alert(X);
+    // "-34.64572", "-58.39917"
+    var map = L.map('map').setView(new L.LatLng(X, Y), 18);
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+}
+
+btn_localizar.addEventListener("click", function(e) {
+    localizar();
+})
